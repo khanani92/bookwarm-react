584861dda16a74e7cc4e876753e88beacebd3fd8
Muhammad-mudassir <muddassir_92@hotmail.com>
2018-05-24 16:29:48 +0500

    semantic ui added

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
 
 const LoginPage = () => (
     <div>
         <h1>Login Page</h1>
-        <Link to="/login">Login</Link>
     </div>
 );
 
