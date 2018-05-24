08bd2cbe8d62c5454cb8dcf9513f7f6c004ca4d4
Muhammad-mudassir <muddassir_92@hotmail.com>
2018-05-24 15:29:08 +0500

    Home page added with route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
 
-
-const App = () => <div>Hello World</div>
+const App = () => (
+    <div>
+        <Route path="/" exact component="{HomePage}">
+    </div>
+)
 
 
 export default App;
